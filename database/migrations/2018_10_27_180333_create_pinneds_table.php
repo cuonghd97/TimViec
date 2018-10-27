f37c29ca807219cf2d212b255ce1622db18aa3ce
cuonghd97 <duccuongdc97@gmail.com>
2018-10-27 18:08:53 +0700

    update

---
+++ database/migrations/2018_10_27_180333_create_pinneds_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePinnedsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pinneds', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('user_id')->nullable();
+            $table->string('post_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pinneds');
+    }
+}
